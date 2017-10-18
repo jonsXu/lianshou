b42200489ecf2b405f0fa4c2f8ba942d3f607a6d
xujingwei <1198513700@qq.com>
2017-10-18 23:05:00 +0800

    add:实现方块停止

--- src/view/game/index.js
+++ src/view/game/index.js
@@ -9,6 +9,8 @@ export default {
       cellWidth:0,//单元块的边长
       matrix: new Array(20),
       ctx:null,//画布对象
+      noWblock:null,//当前正在下落的方块
+      newFlag:true,//是否新生成一个方块
       allWindow:{
         timer:null,
         imgs:[],//加载好的图片
@@ -32,6 +34,7 @@ export default {
       block.XandY = [{
         x:0,y:6
       }]//初始坐标位置
+      self.matrix[block.XandY[0].x][block.XandY[0].y]=0
       return block;
     },
     //初始化画布
@@ -72,34 +75,25 @@ export default {
       })
     },
     //开始游戏
-  run(){
-    console.info(this.allWindow.imgs)
-    console.info("进入到run")
-    var self = this 
-    var h = 0
-    var block = null
-    self.allWindow.timer = setInterval(function(){
-      /**
-       * block.XandY = [{
-        x:6*self.cellWidth,y:0
-      }]/
-       */
-        
-        if(h==0){
-          block = self.createBlock()
-        }
-        self.matrix[block.XandY[0].x][block.XandY[0].y]=0
-        self.reflahCavs()
-        self.matrix[block.XandY[0].x][block.XandY[0].y]=-1
-        block.XandY[0].x++
-        h++
-        if(h==20){
-          h=0
-        }
-        
-    },1000)
-    //this.ctx.drawImage(oimg, 6* self.cellWidth, 0 * self.cellWidth, '20', '20');
-  },
+    run(){
+      console.info(this.allWindow.imgs)
+      console.info("进入到run")
+      var self = this 
+      //var h = 0
+      self.noWblock = self.createBlock()
+      self.reflahCavs()
+      self.allWindow.timer = setInterval(function(){
+        /**
+         * block.XandY = [{
+          x:6*self.cellWidth,y:0
+        }]/
+        */
+          self.dropStop()
+          self.reflahCavs()
+          
+      },1000)
+      //this.ctx.drawImage(oimg, 6* self.cellWidth, 0 * self.cellWidth, '20', '20');
+    },
   /**
    * 预加载图片
    * 
@@ -107,23 +101,36 @@ export default {
    * sw - 屏幕的缩放比例
    * fun - 图片加载完成以后的回调函数
    */
-  loadImgs(imgs,fun){
-    let self = this
-    let l = imgs.length
-    let h = 0
-    imgs.forEach(function(img,index){
-      let imgObject = new Image()
-      imgObject.src = img;
-      imgObject.onload = function(){
-        // this.width = this.width * sw
-        // this.height = this.height * sw
-        self.allWindow.imgs.push(this)
-        h++
-        h>=l&&fun()
+    loadImgs(imgs,fun){
+      let self = this
+      let l = imgs.length
+      let h = 0
+      imgs.forEach(function(img,index){
+        let imgObject = new Image()
+        imgObject.src = img;
+        imgObject.onload = function(){
+          // this.width = this.width * sw
+          // this.height = this.height * sw
+          self.allWindow.imgs.push(this)
+          h++
+          h>=l&&fun()
+        }
+        
+      })
+    },
+    dropStop(){
+      let self = this 
+      if(self.noWblock.XandY[0].x==19 || self.matrix[self.noWblock.XandY[0].x+1][self.noWblock.XandY[0].y]==1){
+        self.matrix[self.noWblock.XandY[0].x][self.noWblock.XandY[0].y]=1
+        self.noWblock = self.createBlock()
+        self.newFlag = true
+      }else {
+          self.matrix[self.noWblock.XandY[0].x][self.noWblock.XandY[0].y]=-1
+          self.noWblock.XandY[0].x++
+          self.matrix[self.noWblock.XandY[0].x][self.noWblock.XandY[0].y]=0
+          self.newFlag = false
       }
-      
-    })
-  },
+    }
   },
   
   mounted(){
